2794b4543a2c9eab3b8356cb20b33bba7bca6a2a
Kunal Patil <kunalpatil970730@gmail.com>
2022-09-26 19:47:31 +0530

    Commmit

--- project/settings.py
+++ project/settings.py
@@ -160,8 +160,8 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
