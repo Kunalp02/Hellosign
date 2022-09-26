45b09c0bb60a69d5387702419117af42967d8d6c
Kunal Patil <kunalpatil970730@gmail.com>
2022-09-26 19:51:12 +0530

    Commmmit

--- accounts/views.py
+++ accounts/views.py
@@ -42,30 +42,30 @@ def callback(request):
 
 
 def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['phone_number']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            username = email.split('@')[0]
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
-            user.phone_number = phone_number
-            user.is_active = True
-            user.save()
+    # if request.method == 'POST':
+    #     form = RegistrationForm(request.POST)
+    #     if form.is_valid():
+    #         first_name = form.cleaned_data['first_name']
+    #         last_name = form.cleaned_data['last_name']
+    #         phone_number = form.cleaned_data['phone_number']
+    #         email = form.cleaned_data['email']
+    #         password = form.cleaned_data['password']
+    #         username = email.split('@')[0]
+    #         user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+    #         user.phone_number = phone_number
+    #         user.is_active = True
+    #         user.save()
 
         
-            messages.success(request, 'Thanks for registering with us')
-            # return redirect('/accounts/login/?command=verification&email='+email)
-            return redirect('login')
-    else:
-        form = RegistrationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/register.html', context)
+    #         messages.success(request, 'Thanks for registering with us')
+    #         # return redirect('/accounts/login/?command=verification&email='+email)
+    #         return redirect('login')
+    # else:
+    #     form = RegistrationForm()
+    # context = {
+    #     'form': form
+    # }
+    return render(request, 'accounts/register.html')
 
 
 
